ffbeb011fd17ed587e5f3910bd3227d23e4ad377
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-01-31 20:13:17 -0500

    Create game.js

---
+++ game.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+resize();
+window.onresize = resize;
